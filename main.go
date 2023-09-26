df66e00509f751a477ad97435b320e2d18e5cf4a
k0ntinuum <k0ntinuum@proton.me>
2023-09-25 23:49:52 -0400

    clean up

--- main.go
+++ main.go
@@ -15,21 +15,19 @@ func main() {
 	if len(os.Args) > 1 {
 		loadHandsFromFile(os.Args[1])
 	}
-	//field.randomize()
+
 	mutField[0].randomize()
 	mutField[1].randomize()
-	//field.randomize()
+
 	rl.InitWindow(int32(cols*pixelWidth), int32(rows*pixelHeight), "Kontinuum : Go")
 	rl.SetTargetFPS(int32(fps))
 	for !rl.WindowShouldClose() {
 		respondToUser()
-		// for _, hand := range hands {
-		// 	field = hand.led(field)
-		// }
+
 		for _, hand := range hands {
 			updateFieldWith(hand)
 		}
-		//canvas.updateWithField(field)
+
 		canvas.updateWithField(mutField[showFieldIndex])
 		rl.BeginDrawing()
 		canvas.plot()
